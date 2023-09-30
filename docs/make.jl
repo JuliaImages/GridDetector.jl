27dbe32842f50b919dbecee0cd36336f938020ef
ashwani rathee <ashwanirathee.work@gmail.com>
2023-10-01 00:55:53 +0530

    Docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,9 +6,21 @@ DocMeta.setdocmeta!(GridDetector, :DocTestSetup, :(using GridDetector); recursiv
 
 makedocs(;
     modules=[GridDetector],
+    authors="Ashwani Rathee",
+    repo="github.com/ashwani-rathee/GridDetector.jl/blob/{commit}{path}#{line}",
     sitename="GridDetector.jl",
+    format=Documenter.HTML(;
+        prettyurls=Base.get(ENV, "CI", "false") == "true",
+        canonical="https://ashwani-rathee.github.io/GridDetector.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
 )
 
 deploydocs(;
     repo="github.com/ashwani-rathee/GridDetector.jl",
-)
+    devbranch="main",
+    push_preview = true
+) 
