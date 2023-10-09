91e32af7811b7789da4bad8b030abd1a7fe78456
ashwani rathee <ashwanirathee.work@gmail.com>
2023-10-09 19:16:52 +0530

    precompile already

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,14 @@
 using PrecompileTools
+using FileIO
+using Colors
 
+println(pwd())
 @setup_workload begin
+    
+    img = rand(Gray, 500,500)
+    
     @compile_workload begin
-        
+        res = process_image(img)
+        draw_rect(img, res,  Gray(1)) 
     end
 end
